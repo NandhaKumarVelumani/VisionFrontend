d1b6eaac0d1baf274f7946fc9b8e95114effa74c
Nandhakumar Velumani <velumaninandhakumar@gmail.com>
2022-02-21 23:09:18 +0530

    tried implementing responsiveness

---
+++ FrontEnd/assets/js/login.js
@@ -0,0 +1,9 @@
+//-----nav links------
+const navLinks = document.getElementById("navLinks");
+
+function showMenu() {
+  navLinks.style.right = "0";
+}
+function hideMenu() {
+  navLinks.style.right = "-200px";
+}
