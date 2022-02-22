d6b6852cef5958310a4363d0ce2539aeb4e352c0
Nandhakumar Velumani <velumaninandhakumar@gmail.com>
2022-02-22 20:23:40 +0530

    made changes in home page

--- FrontEnd/assets/js/home.js
+++ FrontEnd/assets/js/home.js
@@ -1,11 +1,7 @@
-//-----nav links------
-const navLinks = document.getElementById("navLinks");
-const submitBtn = document.querySelector(".btn");
-const allInputs = document.querySelectorAll("input");
-const textarea = document.querySelector("textarea");
 const tabs = document.querySelectorAll(".operations__tab");
 const tabsContainer = document.querySelector(".operations__tab-container");
 const operationsContent = document.querySelectorAll(".operations__content");
+const navLinks = document.getElementById("navLinks");
 
 function showMenu() {
   navLinks.style.right = "0";
@@ -13,14 +9,6 @@ function showMenu() {
 function hideMenu() {
   navLinks.style.right = "-200px";
 }
-
-submitBtn.addEventListener("click", function (e) {
-  e.preventDefault();
-  allInputs.forEach((inps) => {
-    inps.value = "";
-  });
-  textarea.value = "";
-});
 
 tabsContainer.addEventListener("click", (e) => {
   const clicked = e.target.closest(".operations__tab");
