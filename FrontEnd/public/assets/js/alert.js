3b8e008f9cb1b379a93ab558e8e06bac75eec8df
vishalsetti <vishalsetti2000@gmail.com>
2022-03-03 14:16:58 +0530

    added alert.js

---
+++ FrontEnd/public/assets/js/alert.js
@@ -0,0 +1,7 @@
+const button = document.querySelector(".btn");
+const message = "Successful!";
+
+button.addEventListener("click", (e) => {
+    e.preventDefault();
+    swal(message, "", "success");
+})
